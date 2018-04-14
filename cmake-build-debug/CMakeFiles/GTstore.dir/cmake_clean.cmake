652ee9994b0a4dece675428f3e6a531947c37465
Zeyu Chen <chenzy@gatech.edu>
2018-04-13 23:13:57 -0400

    Create

---
+++ cmake-build-debug/CMakeFiles/GTstore.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GTstore.dir/main.cpp.o"
+  "GTstore.pdb"
+  "GTstore"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GTstore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
